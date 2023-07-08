7b99297411dc67a446302ff21bbfaadb5fc1e82e
hyohyo814 <sojaisam@gmail.com>
2023-07-08 15:27:22 -0700

    Missing commits

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,46 +1,44 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('');
 
   const onChange = (event) => {
-    setValue(event.target.value)
-  }
+    setValue(event.target.value);
+  };
 
   const onSubmit = () => {
-    setValue('')
-  }
+    setValue('');
+  };
 
   return {
     type,
     value,
     onChange,
-    onSubmit
-  }
-}
+    onSubmit,
+  };
+};
 
 export const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
+  const [resources, setResources] = useState([]);
 
   useEffect(() => {
     const init = async () => {
-      const res = await axios.get(baseUrl)
-      setResources(res.data)
-    }
-    init()
-  }, [baseUrl, resources])
+      const res = await axios.get(baseUrl);
+      setResources(res.data);
+    };
+    init();
+  }, [baseUrl, resources]);
 
   const create = async (resource) => {
-    const res = await axios.post(baseUrl, resource)
-    return res.data
-  }
+    const res = await axios.post(baseUrl, resource);
+    return res.data;
+  };
 
   const service = {
-    create
-  }
+    create,
+  };
 
-  return [
-    resources, service
-  ]
-}
+  return [resources, service];
+};
